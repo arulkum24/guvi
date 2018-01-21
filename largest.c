e15860305359020a3858dbfdcd981668c0bb9ec4
arulkum24 <35652155+arulkum24@users.noreply.github.com>
2018-01-21 16:48:50 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a=25,b=28,c=14;
+    if(a>b&a>c)
+    printf("a is largest");
+    if(b>a&b>c)
+    printf("b is largest");
+    else
+    printf("c is largest");
+}
