bfbebd08beefbe7dcfa8590f06f1043cb9b0eec4
arulkum24 <35652155+arulkum24@users.noreply.github.com>
2018-02-08 16:03:13 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int m,n;
+printf("enter the number");
+scanf("%d",%m);
+for(n=1;n<=m;n++);
+{
+if(m%n=0)
+}
+printf("\nit is prime number");
+else
+printf("\nit is not prime number");
+return 0;
+}
